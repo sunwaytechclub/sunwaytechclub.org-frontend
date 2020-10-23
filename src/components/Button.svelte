01a451ec4fd97d25aaa38b96557358ff24c2774c
pupubird <rainchai4240@gmail.com>
2020-10-23 17:14:01 +0800

    Add slot fallback

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,44 +1,44 @@
 <script>
-	export let backgroundColor = "var(--purple)";
-	function mounted(obj) {
-		obj.style.backgroundColor = backgroundColor;
-	}
+  export let backgroundColor = "var(--purple)";
+  function mounted(obj) {
+    obj.style.backgroundColor = backgroundColor;
+  }
 </script>
 
 <style>
-	.button {
-		/* Font */
-		font-weight: bold;
-		font-size: var(--p);
+  .button {
+    /* Font */
+    font-weight: bold;
+    font-size: var(--p);
 
-		/* Color */
-		box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-		color: white;
-		border: none;
+    /* Color */
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
+    color: white;
+    border: none;
 
-		/* Layout */
-		padding: 16.5px 27.5px;
-		border-radius: 20px;
-		outline: none;
+    /* Layout */
+    padding: 16.5px 27.5px;
+    border-radius: 20px;
+    outline: none;
 
-		/* Effect */
-		cursor: pointer;
-		transition: all 0.1s ease-in-out;
-		transform: perspective(1000px);
-	}
+    /* Effect */
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+    transform: perspective(1000px);
+  }
 
-	.button:not(:disabled):hover {
-		/* Create zoom in effect on button that is not disabled and hovered */
-		transform: perspective(1000px) translate3d(0, 0, 43px);
-	}
+  .button:not(:disabled):hover {
+    /* Create zoom in effect on button that is not disabled and hovered */
+    transform: perspective(1000px) translate3d(0, 0, 43px);
+  }
 
-	.button:disabled {
-		/* Color */
-		background-color: #e0e0e0 !important;
-		color: #828282;
-	}
+  .button:disabled {
+    /* Color */
+    background-color: #e0e0e0 !important;
+    color: #828282;
+  }
 </style>
 
 <button use:mounted on:click class="button" {...$$restProps}>
-	<slot>Click me</slot>
+  <slot>Click me</slot>
 </button>
