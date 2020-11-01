ff018706afa509dcd6c92044b768095bb298ef05
yeo wen <yeoyeewen@gmail.com>
2020-11-01 18:57:02 +0800

    commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav 
+	<!-- some css here --!>
+</style>
+
+<nav>
+	<ul>
+	<!-- something after we login --!>
+	</ul>
+</nav>
--- src/components/stores.js
+++ src/components/stores.js
@@ -1,5 +1,7 @@
 import { writable } from 'svelte/store';
+import {writable} from 'svelte/store';
 
 // Stores for router
 export const Route = writable(undefined);
 export const params = writable(undefined);
+export const currentUser = writable(null);
