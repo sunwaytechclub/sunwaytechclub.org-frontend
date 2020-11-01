6af401c0953889e5c92eabdfe48fed70c3a4b891
yeo wen <yeoyeewen@gmail.com>
2020-11-01 21:35:04 +0800

    commit

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,7 +1,5 @@
 import { writable } from 'svelte/store';
-import {writable} from 'svelte/store';
 
 // Stores for router
 export const Route = writable(undefined);
 export const params = writable(undefined);
-export const currentUser = writable(null);
